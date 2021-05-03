6879fded1b8c5892c05d8325b204fee13932c140
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2021-05-03 11:20:14 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,68 @@
+import dash
+import dash_table
+from dash.dependencies import Input, Output
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
 import matt99
 from matt99 import INPUT, OUTPUT
 import simpy
 
-import numpy as np
 
+app = dash.Dash(__name__, serve_locally=True)
+app.title = "Matt99"
+
+
+
+app.layout = html.Div([
+
+    html.H6('General data'),
+    dash_table.DataTable(
+        id='general-table',
+        editable=True,
+        style_cell={"textAlign" : "center"},
+        style_table={'width': '20px'},
+        style_header={'fontWeight': 'bold'},
+        columns=[
+            {"name": "SimulationTime[days]", "id": "SimulationTime"},
+            {"name": "InitialFilling[%]", "id": "InitialFilling"},
+            {"name": "UploadingTime[s]", "id": "UploadingTime"}
+        ],
+        data=[{"SimulationTime" : 5, "InitialFilling" : .5, "UploadingTime" : 120}]
+    ),
+    html.Br(),
+    dbc.Row([html.H6('Depots data'), html.Button("Add",id="add-depot",n_clicks=0), html.Button("Remove",id="pop-depot",n_clicks=0)]),
+    dash_table.DataTable(
+        id='depots-table',
+        editable=True,
+        style_cell={"textAlign" : "center"},
+        style_table={'width': '20px'},
+        style_header={'fontWeight': 'bold'},
+        columns=[
+            {"name": "Type", "id": "Type", "presentation" : "dropdown"},
+            {"name": "Probability", "id": "Probability"},
+            {"name": "ShuttleSpeed[m/s]", "id": "ShuttleSpeed"},
+            {"name": "ShuttleAcceleration[m/s2]", "id": "ShuttleAcceleration"}
+        ],
+        dropdown={"Type" : {"options" : [{"label" : "INPUT", "value" : INPUT}, {"label" : "OUTPUT", "value" : OUTPUT}]}},
+        data=[
+            {"Type" : "INPUT", "Probability" : 0.25,"ShuttleSpeed" : 2.0, "ShuttleAcceleration" : .5},
+            {"Type" : "OUTPUT", "Probability" : 0.25,"ShuttleSpeed" : 2.0, "ShuttleAcceleration" : .5},
+            {"Type" : "INPUT", "Probability" : 0.25,"ShuttleSpeed" : 2.0, "ShuttleAcceleration" : .5},
+            {"Type" : "OUTPUT", "Probability" : 0.25,"ShuttleSpeed" : 2.0, "ShuttleAcceleration" : .5}
+        ]
+    ),
+
+
+])
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0', port=8000)
+
+
+"""
 if __name__ == "__main__":
 
     simTime = 140_000
@@ -36,4 +95,4 @@ if __name__ == "__main__":
     for job in sim.done:
         print(job.kind, job.arrival, " - ", job.history["END"] - job.history["START"])
 
-    print(len(sim.done), len(sim.wasted))
+"""
